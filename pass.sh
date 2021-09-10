6df71774c77503b6cf983945715154a4951ae14e
Eden Ohana <edeno@bynet.co.il>
2021-09-10 14:20:33 +0300

    Add ex

---
+++ pass.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+password=$1
+error='\e[31m';
+successful='\e[32m';
+####################################################################################################
+if [ ${#password} -lt 10 ];then
+    echo -e $error "\t \n '$password' Password value must be at least 10 characters long"
+else
+    echo "good"
+fi       
+###########################################################################################
+echo $password | grep "[0-9]"| grep "[a-z]" | grep "[A-Z]"
+if [[ $? -ne 0 ]];then
+echo "The password must contain letters and numbers"
+    exit 1;
+
+else
+echo -e $successful"The test passed the password correctly"
+exit 0;
+fi
+#############################################################################################
